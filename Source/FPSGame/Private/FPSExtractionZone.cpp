f30734adddb679b31359b6b6fbc94b48ad022ab2
Pere Massaguer Vila <pmassaguervila@gmail.com>
2022-04-27 13:20:55 +0200

    [Extraction Zone] Implemented win conditions with sfx

---
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSExtractionZone.h"
+#include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"
+#include "FPSCharacter.h"
+#include "FPSGameMode.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	OverlapComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComponent"));
+	OverlapComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComponent->SetBoxExtent(FVector(200.0f));
+	OverlapComponent->SetHiddenInGame(true);
+	RootComponent = OverlapComponent;
+
+	OverlapComponent->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::OverlapHandle);
+
+	DecalComponent = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComponent"));
+	DecalComponent->DecalSize = FVector(200.0f);
+	DecalComponent->SetupAttachment(RootComponent);
+}
+
+void AFPSExtractionZone::OverlapHandle(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AFPSCharacter* character = Cast<AFPSCharacter>(OtherActor);
+	if (character == nullptr) return;
+	
+	if (character->bIsCarryingObjective)
+	{
+		AFPSGameMode* gameMode = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+		if (gameMode)
+		{
+			gameMode->CompleteMission(character);
+		}
+		UGameplayStatics::PlaySound2D(this, ObjectiveCompletedSound);
+	}
+	else
+	{
+		UGameplayStatics::PlaySound2D(this, ObjectiveMissingSound);
+	}
+}
