846c7527b13ed91a7ab331c565262551e3b1239a
Pere Massaguer Vila <pmassaguervila@gmail.com>
2022-04-28 21:14:39 +0200

    Solve project warnings

--- Source/FPSGame/Private/FPSObjectiveActor.cpp
+++ Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -22,7 +22,7 @@ AFPSObjectiveActor::AFPSObjectiveActor()
 	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 	SphereComp->SetupAttachment(MeshComp);
 
-	SetReplicates(true);
+	bReplicates = true;
 }
 
 // Called when the game starts or when spawned
